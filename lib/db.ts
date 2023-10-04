d46eb181a6b9f8df4383553ad01bd1e79a6e7fd0
Vishrudh Raj.R.S <vishrudhraj.r.s@Vishrudhs-MacBook-Pro.local>
2023-10-04 20:46:47 +0530

    vercel2

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
+declare global {
+  // eslint-disable-next-line no-var, no-unused-vars
+  var prisma: PrismaClient;
+}
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
